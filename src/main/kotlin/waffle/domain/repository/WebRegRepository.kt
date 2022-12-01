5378fd6d26bfd659e76df0396ebd37849ce824f6
tmyksj <33417830+tmyksj@users.noreply.github.com>
2022-12-01 19:38:55 +0900

    Create `WebRegRepository`

---
+++ src/main/kotlin/waffle/domain/repository/WebRegRepository.kt
@@ -0,0 +1,25 @@
+package waffle.domain.repository
+
+import waffle.domain.entity.WebReg
+import java.util.*
+
+/**
+ * Repository for WebReg.
+ */
+interface WebRegRepository {
+
+    /**
+     * Retrieves an entity by its id.
+     * @param id
+     * @return the entity with the given id or null if none found.
+     */
+    fun findById(id: UUID): WebReg?
+
+    /**
+     * Saves a given entity.
+     * @param entity
+     * @return the saved entity.
+     */
+    fun save(entity: WebReg): WebReg
+
+}
