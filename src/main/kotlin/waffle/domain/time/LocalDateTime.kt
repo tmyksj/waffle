c84bc266beccc9e970a9235714ea1e6ac81a02f6
tmyksj <33417830+tmyksj@users.noreply.github.com>
2022-12-10 09:45:49 +0900

    Create `now` which returns `LocalDateTime` with a microseconds precision

---
+++ src/main/kotlin/waffle/domain/time/LocalDateTime.kt
@@ -0,0 +1,12 @@
+package waffle.domain.time
+
+import java.time.LocalDateTime
+
+/**
+ * Obtains the current date-time from the system clock in the default time-zone.
+ */
+fun now(): LocalDateTime {
+    return LocalDateTime.now().let {
+        it.minusNanos(it.nano % 1000L)
+    }
+}
---
+++ src/test/kotlin/waffle/domain/time/LocalDateTimeTests.kt
@@ -0,0 +1,25 @@
+package waffle.domain.time
+
+import org.assertj.core.api.SoftAssertions
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import java.time.LocalDateTime
+
+@SpringBootTest
+class LocalDateTimeTests {
+
+    @Test
+    fun now_returns_LocalDateTime_with_a_microseconds_precision() {
+        val a: LocalDateTime = LocalDateTime.now()
+            .let { it.minusNanos(it.nano % 1000L) }
+        val b: LocalDateTime = now()
+        val c: LocalDateTime = LocalDateTime.now()
+
+        SoftAssertions.assertSoftly {
+            it.assertThat(b).isAfterOrEqualTo(a)
+            it.assertThat(b).isBeforeOrEqualTo(c)
+            it.assertThat(b.nano % 1000).isEqualTo(0)
+        }
+    }
+
+}
