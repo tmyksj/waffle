43136d6d7b2169efa7e47c3aeb20f802e9c65518
tmyksj <33417830+tmyksj@users.noreply.github.com>
2022-12-11 08:56:39 +0900

    Update KDoc

--- src/main/kotlin/waffle/domain/time/LocalDateTime.kt
+++ src/main/kotlin/waffle/domain/time/LocalDateTime.kt
@@ -4,6 +4,7 @@ import java.time.LocalDateTime
 
 /**
  * Obtains the current date-time from the system clock in the default time-zone.
+ * @return the current date-time using the system clock and default time-zone.
  */
 fun now(): LocalDateTime {
     return LocalDateTime.now().let {
