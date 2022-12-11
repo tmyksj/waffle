8f714242dc51abf6e283f2a0dfa4c8d1e96221b6
tmyksj <33417830+tmyksj@users.noreply.github.com>
2022-12-11 09:11:05 +0900

    Create `WebRegFactory`

---
+++ src/test/kotlin/waffle/test/factory/WebRegFactory.kt
@@ -0,0 +1,74 @@
+package waffle.test.factory
+
+import org.springframework.boot.test.context.TestComponent
+import waffle.domain.entity.WebReg
+import waffle.domain.time.now
+import java.net.URL
+import java.time.LocalDateTime
+import java.util.*
+
+/**
+ * Component for building a WebReg entity.
+ */
+@TestComponent
+class WebRegFactory {
+
+    /**
+     * Returns a new entity.
+     */
+    fun build(
+        id: UUID = UUID.randomUUID(),
+        cases: List<WebReg.Case> = listOf(
+            WebReg.Case(
+                expected = URL("https://example.com"),
+                actual = URL("https://example.com"),
+            ),
+        ),
+        result: String? = null,
+        state: WebReg.State = WebReg.State.Ready,
+        startedDate: LocalDateTime? = null,
+        completedDate: LocalDateTime? = null,
+        failedDate: LocalDateTime? = null,
+        createdDate: LocalDateTime = now(),
+        lastModifiedDate: LocalDateTime = createdDate,
+    ): WebReg {
+        return WebReg(
+            id = id,
+            cases = cases,
+            result = result,
+            state = state,
+            startedDate = startedDate,
+            completedDate = completedDate,
+            failedDate = failedDate,
+            createdDate = createdDate,
+            lastModifiedDate = lastModifiedDate,
+        )
+    }
+
+    /**
+     * Returns a modified entity based on a given entity.
+     */
+    fun modify(
+        entity: WebReg,
+        cases: List<WebReg.Case> = entity.cases,
+        result: String? = entity.result,
+        state: WebReg.State = entity.state,
+        startedDate: LocalDateTime? = entity.startedDate,
+        completedDate: LocalDateTime? = entity.completedDate,
+        failedDate: LocalDateTime? = entity.failedDate,
+        createdDate: LocalDateTime = entity.createdDate,
+        lastModifiedDate: LocalDateTime = now(),
+    ): WebReg {
+        return entity.copy(
+            cases = cases,
+            result = result,
+            state = state,
+            startedDate = startedDate,
+            completedDate = completedDate,
+            failedDate = failedDate,
+            createdDate = createdDate,
+            lastModifiedDate = lastModifiedDate,
+        )
+    }
+
+}
