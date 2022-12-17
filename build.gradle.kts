6eb1ba2636dd01e7d1131bfc786b24af1b6125f3
tmyksj <33417830+tmyksj@users.noreply.github.com>
2022-12-17 09:03:56 +0900

    Add `org.seleniumhq.selenium:selenium-java:4.7.2`

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.flywaydb:flyway-mysql")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.seleniumhq.selenium:selenium-java:4.7.2")
     implementation("org.springframework.boot:spring-boot-starter-batch")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
