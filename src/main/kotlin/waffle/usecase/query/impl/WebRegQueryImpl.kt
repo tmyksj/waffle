c94788d73e90e1996b9217235efef0a3607ef9b0
tmyksj <33417830+tmyksj@users.noreply.github.com>
2022-12-31 09:38:59 +0900

    Annotate `org.springframework.transaction.annotation.Transactional`

--- src/main/kotlin/waffle/usecase/query/impl/WebRegQueryImpl.kt
+++ src/main/kotlin/waffle/usecase/query/impl/WebRegQueryImpl.kt
@@ -1,12 +1,14 @@
 package waffle.usecase.query.impl
 
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 import waffle.domain.entity.WebReg
 import waffle.domain.repository.WebRegRepository
 import waffle.usecase.query.WebRegQuery
 import java.util.*
 
 @Component
+@Transactional
 class WebRegQueryImpl(
         private val webRegRepository: WebRegRepository,
 ) : WebRegQuery {
