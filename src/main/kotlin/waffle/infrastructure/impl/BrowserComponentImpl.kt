4ee6d40919f4216c37d09cf1647acde28911c8ef
tmyksj <33417830+tmyksj@users.noreply.github.com>
2023-01-01 10:27:55 +0900

    Fix to close `WebDriver`

--- src/main/kotlin/waffle/infrastructure/impl/BrowserComponentImpl.kt
+++ src/main/kotlin/waffle/infrastructure/impl/BrowserComponentImpl.kt
@@ -6,29 +6,32 @@ import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.springframework.stereotype.Component
 import waffle.component.BrowserComponent
+import java.io.Closeable
 import java.net.URL
 
 @Component
 class BrowserComponentImpl : BrowserComponent {
 
     override fun captureScreenshot(
-        url: URL,
-        width: Int?,
-        height: Int?,
+            url: URL,
+            width: Int?,
+            height: Int?,
     ): ByteArray {
         return ChromeDriver(ChromeOptions().setHeadless(true)).run {
-            manage().window().size =
-                Dimension(width ?: BrowserComponent.DEFAULT_WIDTH, height ?: BrowserComponent.DEFAULT_HEIGHT)
+            Closeable { close() }.use {
+                manage().window().size =
+                        Dimension(width ?: BrowserComponent.DEFAULT_WIDTH, height ?: BrowserComponent.DEFAULT_HEIGHT)
 
-            get(url.toString())
+                get(url.toString())
 
-            val scrollHeight: Any = executeScript("return document.body.scrollHeight;")
-            check(scrollHeight is Number)
+                val scrollHeight: Any = executeScript("return document.body.scrollHeight;")
+                check(scrollHeight is Number)
 
-            manage().window().size =
-                Dimension(width ?: BrowserComponent.DEFAULT_WIDTH, height ?: scrollHeight.toInt())
+                manage().window().size =
+                        Dimension(width ?: BrowserComponent.DEFAULT_WIDTH, height ?: scrollHeight.toInt())
 
-            getScreenshotAs(OutputType.BYTES)
+                getScreenshotAs(OutputType.BYTES)
+            }
         }
     }
 
