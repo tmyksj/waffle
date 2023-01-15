5f2ff70e107a9435314660791dc8adf54cb91ba8
tmyksj <33417830+tmyksj@users.noreply.github.com>
2023-01-15 14:38:48 +0900

    Delete temp directory certainly

--- src/main/kotlin/waffle/infrastructure/impl/ReportingComponentImpl.kt
+++ src/main/kotlin/waffle/infrastructure/impl/ReportingComponentImpl.kt
@@ -3,6 +3,7 @@ package waffle.infrastructure.impl
 import org.springframework.stereotype.Component
 import waffle.component.ContentTypeComponent
 import waffle.component.ReportingComponent
+import java.io.Closeable
 import java.io.FileOutputStream
 import java.nio.file.Path
 import java.util.zip.ZipEntry
@@ -17,48 +18,50 @@ class ReportingComponentImpl(
     @OptIn(ExperimentalPathApi::class)
     override fun compareImages(a: List<ByteArray>, b: List<ByteArray>): ByteArray {
         return createTempDirectory().run {
-            path("regconfig.json").writeText(
-                """
-                    {
-                        "core": {
-                            "actualDir": "actual",
-                            "workingDir": ".reg",
-                            "ximgdiff": {
-                                "invocationType": "none"
+            Closeable { toFile().deleteRecursively() }.use { _ ->
+                path("regconfig.json").writeText(
+                    """
+                        {
+                            "core": {
+                                "actualDir": "actual",
+                                "workingDir": ".reg",
+                                "ximgdiff": {
+                                    "invocationType": "none"
+                                }
                             }
                         }
-                    }
-                """.trimIndent(),
-            )
+                    """.trimIndent(),
+                )
 
-            path(".reg", "expected").createDirectories()
+                path(".reg", "expected").createDirectories()
 
-            a.forEachIndexed { index, it ->
-                path(".reg", "expected", "${index + 1}${contentTypeComponent.guessExtension(it)}").writeBytes(it)
-            }
-
-            path("actual").createDirectories()
-
-            b.forEachIndexed { index, it ->
-                path("actual", "${index + 1}${contentTypeComponent.guessExtension(it)}").writeBytes(it)
-            }
-
-            ProcessBuilder("reg-suit", "--config", "regconfig.json", "compare")
-                .directory(toFile())
-                .start()
-                .waitFor()
-
-            ZipOutputStream(FileOutputStream(path(".reg.zip").toFile())).use { zipOutputStream ->
-                val startIndex: Int = path(".reg").toString().length
-
-                path(".reg").walk().forEach {
-                    zipOutputStream.putNextEntry(ZipEntry(it.toString().substring(startIndex)))
-                    zipOutputStream.write(it.readBytes())
-                    zipOutputStream.closeEntry()
+                a.forEachIndexed { index, it ->
+                    path(".reg", "expected", "${index + 1}${contentTypeComponent.guessExtension(it)}").writeBytes(it)
                 }
-            }
 
-            path(".reg.zip").readBytes().also { toFile().deleteRecursively() }
+                path("actual").createDirectories()
+
+                b.forEachIndexed { index, it ->
+                    path("actual", "${index + 1}${contentTypeComponent.guessExtension(it)}").writeBytes(it)
+                }
+
+                ProcessBuilder("reg-suit", "--config", "regconfig.json", "compare")
+                    .directory(toFile())
+                    .start()
+                    .waitFor()
+
+                ZipOutputStream(FileOutputStream(path(".reg.zip").toFile())).use { zipOutputStream ->
+                    val startIndex: Int = path(".reg").toString().length
+
+                    path(".reg").walk().forEach {
+                        zipOutputStream.putNextEntry(ZipEntry(it.toString().substring(startIndex)))
+                        zipOutputStream.write(it.readBytes())
+                        zipOutputStream.closeEntry()
+                    }
+                }
+
+                path(".reg.zip").readBytes()
+            }
         }
     }
 
