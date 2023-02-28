9a96f09ab08218a473ff9e0fd615ceb611ad3a08
tmyksj <33417830+tmyksj@users.noreply.github.com>
2023-02-28 19:33:05 +0900

    Define a foreign key

--- src/main/resources/db/migration/V5__Create_Table_WebCheckpoint.sql
+++ src/main/resources/db/migration/V5__Create_Table_WebCheckpoint.sql
@@ -9,7 +9,8 @@ create table wf_web_checkpoint
     created_date       datetime(6) not null default 0,
     last_modified_date datetime(6) not null default 0,
     version            bigint      not null,
-    primary key (id)
+    primary key (id),
+    foreign key (wf_web_flow_id) references wf_web_flow (id) on delete cascade on update cascade
 ) engine = InnoDB
   default charset utf8mb4;
 
@@ -20,6 +21,7 @@ create table wf_web_checkpoint_snapshot
     resource             text        not null,
     width_px             bigint      not null,
     screenshot           varchar(36) not null,
-    primary key (wf_web_checkpoint_id, idx)
+    primary key (wf_web_checkpoint_id, idx),
+    foreign key (wf_web_checkpoint_id) references wf_web_checkpoint (id) on delete cascade on update cascade
 ) engine = InnoDB
   default charset utf8mb4;
