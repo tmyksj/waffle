30a9e99f8da3f37fd6957ba690abe92d8b8e777e
tmyksj <33417830+tmyksj@users.noreply.github.com>
2023-03-05 20:11:01 +0900

    Add state methods

--- src/test/kotlin/waffle/domain/entity/WebCheckpointTests.kt
+++ src/test/kotlin/waffle/domain/entity/WebCheckpointTests.kt
@@ -1,7 +1,11 @@
 package waffle.domain.entity
 
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.SoftAssertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import waffle.core.time.now
@@ -18,6 +22,30 @@ class WebCheckpointTests {
 
     @Autowired
     private lateinit var webSnapshotFactory: WebSnapshotFactory
+
+    @MethodSource
+    @ParameterizedTest
+    fun isReady(state: WebCheckpoint.State, expected: Boolean) {
+        Assertions.assertThat(webCheckpointFactory.build(state = state).isReady).isEqualTo(expected)
+    }
+
+    @MethodSource
+    @ParameterizedTest
+    fun isStarted(state: WebCheckpoint.State, expected: Boolean) {
+        Assertions.assertThat(webCheckpointFactory.build(state = state).isStarted).isEqualTo(expected)
+    }
+
+    @MethodSource
+    @ParameterizedTest
+    fun isCompleted(state: WebCheckpoint.State, expected: Boolean) {
+        Assertions.assertThat(webCheckpointFactory.build(state = state).isCompleted).isEqualTo(expected)
+    }
+
+    @MethodSource
+    @ParameterizedTest
+    fun isFailed(state: WebCheckpoint.State, expected: Boolean) {
+        Assertions.assertThat(webCheckpointFactory.build(state = state).isFailed).isEqualTo(expected)
+    }
 
     @Test
     fun transition_to_the_state_Started() {
@@ -61,6 +89,50 @@ class WebCheckpointTests {
             it.assertThat(actual.failedDate).isAfterOrEqualTo(now)
             it.assertThat(actual.lastModifiedDate).isAfter(entity.lastModifiedDate)
         }
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun isReady(): List<Arguments> {
+            return listOf(
+                Arguments.of(WebCheckpoint.State.Ready, true),
+                Arguments.of(WebCheckpoint.State.Started, false),
+                Arguments.of(WebCheckpoint.State.Completed, false),
+                Arguments.of(WebCheckpoint.State.Failed, false),
+            )
+        }
+
+        @JvmStatic
+        fun isStarted(): List<Arguments> {
+            return listOf(
+                Arguments.of(WebCheckpoint.State.Ready, false),
+                Arguments.of(WebCheckpoint.State.Started, true),
+                Arguments.of(WebCheckpoint.State.Completed, false),
+                Arguments.of(WebCheckpoint.State.Failed, false),
+            )
+        }
+
+        @JvmStatic
+        fun isCompleted(): List<Arguments> {
+            return listOf(
+                Arguments.of(WebCheckpoint.State.Ready, false),
+                Arguments.of(WebCheckpoint.State.Started, false),
+                Arguments.of(WebCheckpoint.State.Completed, true),
+                Arguments.of(WebCheckpoint.State.Failed, false),
+            )
+        }
+
+        @JvmStatic
+        fun isFailed(): List<Arguments> {
+            return listOf(
+                Arguments.of(WebCheckpoint.State.Ready, false),
+                Arguments.of(WebCheckpoint.State.Started, false),
+                Arguments.of(WebCheckpoint.State.Completed, false),
+                Arguments.of(WebCheckpoint.State.Failed, true),
+            )
+        }
+
     }
 
 }
