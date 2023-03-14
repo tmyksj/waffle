1773969c80a2a5c45dbef9af1ea50577155fa421
tmyksj <33417830+tmyksj@users.noreply.github.com>
2023-03-14 19:53:48 +0900

    Add `org.seleniumhq.selenium:selenium-http-jdk-client:4.7.2`

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     implementation("org.flywaydb:flyway-mysql")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.seleniumhq.selenium:selenium-http-jdk-client:4.7.2")
     implementation("org.seleniumhq.selenium:selenium-java:4.7.2")
     implementation("org.springframework.boot:spring-boot-starter-batch")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
