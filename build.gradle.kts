84362654b3c2926fd9f9aaf921caa27ca9df834e
tmyksj <33417830+tmyksj@users.noreply.github.com>
2023-03-26 10:31:14 +0900

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.0"
+    id("org.springframework.boot") version "3.0.5"
     id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.7.21"
-    kotlin("plugin.spring") version "1.7.21"
-    kotlin("plugin.jpa") version "1.7.21"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.spring") version "1.7.22"
+    kotlin("plugin.jpa") version "1.7.22"
 }
 
 group = "waffle"
@@ -26,13 +26,13 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.apache.tika:tika-core:2.6.0")
+    implementation("org.apache.tika:tika-core:2.7.0")
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-mysql")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.seleniumhq.selenium:selenium-http-jdk-client:4.7.2")
-    implementation("org.seleniumhq.selenium:selenium-java:4.7.2")
+    implementation("org.seleniumhq.selenium:selenium-http-jdk-client:4.8.2")
+    implementation("org.seleniumhq.selenium:selenium-java:4.8.2")
     implementation("org.springframework.boot:spring-boot-starter-batch")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
