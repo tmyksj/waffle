322927762d92cf9c3a0ad06c9d3758ca89e65b1e
tmyksj <33417830+tmyksj@users.noreply.github.com>
2023-04-01 19:46:37 +0900

    Configure session management

--- src/main/kotlin/waffle/configuration/WebSecurityConfiguration.kt
+++ src/main/kotlin/waffle/configuration/WebSecurityConfiguration.kt
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -14,6 +15,10 @@ class WebSecurityConfiguration {
     fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
         httpSecurity.authorizeHttpRequests {
             it.anyRequest().permitAll()
+        }
+
+        httpSecurity.sessionManagement {
+            it.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
         }
 
         return httpSecurity.build()
