c134e14fdf9b25b220907bf8491613be9e86d82c
tmyksj <33417830+tmyksj@users.noreply.github.com>
2023-05-23 19:55:52 +0900

    Delete `deleteAllByWebCheckpointId`

--- src/main/kotlin/waffle/infrastructure/jpa/repository/WebCheckpointSnapshotJpaRepository.kt
+++ src/main/kotlin/waffle/infrastructure/jpa/repository/WebCheckpointSnapshotJpaRepository.kt
@@ -1,7 +1,6 @@
 package waffle.infrastructure.jpa.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
@@ -14,16 +13,6 @@ import waffle.infrastructure.jpa.entity.WebCheckpointSnapshotJpaEntity
 @Transactional
 interface WebCheckpointSnapshotJpaRepository :
     JpaRepository<WebCheckpointSnapshotJpaEntity, WebCheckpointSnapshotJpaEntity.Id> {
-
-    /**
-     * Deletes all instances with the given IDs.
-     *
-     * @param webCheckpointIds
-     * @return
-     */
-    @Modifying
-    @Query("delete from wf_web_checkpoint_snapshot w where w.id.webCheckpointId in ?1")
-    fun deleteAllByWebCheckpointId(webCheckpointIds: Iterable<String>)
 
     /**
      * Returns all instances with the given IDs.
